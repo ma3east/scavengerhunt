3d5d60445e92ce1853dd06702effefd77b92258a
jacprada <jacopo.prada@gmail.com>
2015-09-11 16:15:41 +0100

    joined hunt added

--- public/js/controllers/hunts.js
+++ public/js/controllers/hunts.js
@@ -5,25 +5,34 @@ HuntsController.$inject = ['Hunt', '$state']
 function HuntsController (Hunt, $state) {
 
   var self = this;
+
   self.newHunt
+
   self.all = Hunt.query();
+
   self.showHunt = function (hunt) {
-  Hunt.get({ id: hunt._id }, function (response) {
-    self.hunt = response;
-    $state.go('showHunt')
-  });
+    Hunt.get({ id: hunt._id }, function (response) {
+      self.hunt = response;
+      $state.go('showHunt')
+    });
 
-}
+  }
 
-self.createHunt = function () {
-  Hunt.save(self.newHunt, function (response) {
-    self.showHunt(response);
-  });
-}
-self.deleteHunt = function (hunt) {
+  self.createHunt = function () {
+    Hunt.save(self.newHunt, function (response) {
+      self.showHunt(response);
+    });
+  }
+
+  self.deleteHunt = function (hunt) {
   // Hunt.delete(hunt._id, function (response) {
   //   console.log(response)
   // } )
 }
+
+  self.joinHunt = function(hunt) {
+    data = { hunt_id: hunt._id, joinedHunt: { hunt_id: hunt._id, user_id: '' } } 
+    Hunt.joined(data)
+  }
 
 }
