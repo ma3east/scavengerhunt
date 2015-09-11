da3661d65127931d078c0019448b519fc5f27575
evanjmg <evanjmg@gmail.com>
2015-09-11 13:03:02 +0100

    setup scripts

--- app.js
+++ app.js
@@ -10,8 +10,16 @@ app.use(bodyParser.urlencoded({extended: false}));
 var databaseURL = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/scavenger-hunt'
 mongoose.connect(databaseURL);
 
+
+// Setting view folder for single index.html file
 app.set("views", "./public");
 app.engine('html', require('ejs').renderFile);
+
+// Serve all js, css, html from the public folder
+app.use(express.static(__dirname + '/public'));
+
+// Serving bower_components from root. Might change to public later
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 app.use(logger('dev'));
 
