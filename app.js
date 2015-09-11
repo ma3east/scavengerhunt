f2a53041071b23c2c9575efed5b9f7e743e8b184
junedev <catherinejune86@gmail.com>
2015-09-11 11:32:30 +0100

    style.scss gets compiled into style.css

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var logger = require("morgan");
+var sassMiddleware = require('node-sass-middleware');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -9,8 +10,16 @@ app.set("views", "./public");
 app.engine('html', require('ejs').renderFile);
 
 app.use(logger('dev'));
-app.use(express.static(__dirname + '/public'));
 
+app.use(sassMiddleware({
+  src: __dirname + '/scss', 
+  dest: __dirname + '/public/css',
+  debug: true,
+  outputStyle: 'compressed',
+  prefix: "/css"
+  }),
+  express.static(__dirname + '/public')
+)
 
 app.use(require('./controllers'));
 
