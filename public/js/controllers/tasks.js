3d8adaf8a03a4af048c9868123aca23048d9f77c
jacprada <jacopo.prada@gmail.com>
2015-09-11 16:11:16 +0100

    completed task added

--- public/js/controllers/tasks.js
+++ public/js/controllers/tasks.js
@@ -1,8 +1,8 @@
 angular.module('scavengerHunt')
   .controller('TasksController', TasksController);
 
-TasksController.$inject = ['Task'];
-function TasksController(Task){
+TasksController.$inject = ['Task', 'CurrentUser'];
+function TasksController(Task, CurrentUser){
   var self = this;
 
   self.task = {}
@@ -21,10 +21,15 @@ function TasksController(Task){
     })
   };
 
-  this.deleteTask = function(task){
+  self.deleteTask = function(task){
     Task.delete({id: task._id});
     var index = self.tasks.indexOf(task);
     self.tasks.splice(index, 1);
   };
+
+  self.completeTask = function(task){
+    data = { task_id: task._id, completedTask: { task_id: task._id, user_id: '' } } 
+    Task.completed(data)
+  }
 
 }
