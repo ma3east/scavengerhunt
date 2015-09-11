4e5e83db7c399f61470aa22440cdf8e3cc39c17c
junedev <catherinejune86@gmail.com>
2015-09-11 11:10:45 +0100

    index moved to public

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require("morgan");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.set("views", "./views");
+app.set("views", "./public");
 app.engine('html', require('ejs').renderFile);
 
 app.use(logger('dev'));
@@ -14,4 +14,4 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(require('./controllers'));
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 3000);
