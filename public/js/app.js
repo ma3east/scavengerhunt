104f3329d1eafba1a122858f24c137606cd784ea
jacprada <jacopo.prada@gmail.com>
2015-09-11 13:42:38 +0100

    some code

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,30 @@
-angular.module('scavengerHunt', ['ui.router', 'angular-jwt' ])
+// angular
+//   .module('scavengerHunt', ['ngResource', 'ui.router', 'angular-jwt'])
+//   .config(MainRouter);
+
+// angular
+//   .module('scavengerHunt')
+//   .run(function($http, $window) {
+
+//     var token = $window.localStorage.getItem('token');
+//     $http.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+
+//   })
+
+// // Setup the routing with ui.router
+// function MainRouter($stateProvider, $urlRouterProvider){
+//   $stateProvider
+//     .state('join', {
+//       url: "/join",
+//       templateUrl: "js/views/join.html"
+//     })
+//     .state('authorize', {
+//       url: "/authorize",
+//       templateUrl: "js/views/authorize.html"
+//     })
+//     .state('disappear', {
+//       url: "/"
+//     });
+
+//   $urlRouterProvider.otherwise("/");
+// }
