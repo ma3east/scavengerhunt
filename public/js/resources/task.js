31f4d59399448f8930c3fd3fd3c9d0335ad115ae
jacprada <jacopo.prada@gmail.com>
2015-09-11 14:38:31 +0100

    added tasks model and controller

---
+++ public/js/resources/task.js
@@ -0,0 +1,17 @@
+angular
+  .module('scavengerHunt')
+  .factory('Task', Task);
+
+Task.$inject = ['$resource'];
+function Task ($resource) {
+
+ var url = 'http://localhost:3000/api/tasks/'
+
+ var TaskResource = $resource(url + ':id',
+    {id: '@_id'},
+    { 'update': { method: 'PUT' }
+ });
+
+ return TaskResource;
+
+}
