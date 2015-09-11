46ed2d3c6a0dbb45765d364bf0f29944d790bd34
Hassanmir92 <hassanmir92@gmail.com>
2015-09-11 12:07:37 +0100

    Added user model

---
+++ models/user.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
+
+var UserSchema = mongoose.Schema({
+  full_name: {type: String, required: true },
+  email: {type: String, required: true, unique: true},
+  password: {type: String, require: true }
+})
+
+UserSchema.methods.encrypt = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+UserSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+}
+
+module.exports = mongoose.model("User", UserSchema);
