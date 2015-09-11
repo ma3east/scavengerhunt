b9343d29e92434113cbe95936a69a0f0c05b42ea
eisacke <emily@isacke.com>
2015-09-11 17:47:41 +0100

    Open and closed tasks

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -26,21 +26,30 @@ router.get('/', function(req, res){
 
 router.post('/tasks', function(req, res){
   Hunt.findById(req.body.hunt_id)
-    .populate('tasks')
-    .exec(populateCompleted);
+  .populate('tasks')
+  .exec(populateCompleted);
+  function populateCompleted(error, hunt){
+    Hunt.populate(hunt, {
+      path: 'tasks.completedTasks',
+      model: 'CompletedTask'
+    },function(error, hunt){
 
-    function populateCompleted(error, hunt){
-      console.log(hunt);
-      Hunt.populate(hunt, {
-        path: 'tasks.completedTasks',
-        model: 'CompletedTask'
-      },function(error, hunt){
-        var result = { completed: [], open: [] };
-        console.log(hunt);
-        return res.status(200).send(hunt);
+      var result = { completed: [], open: [] };
+
+      hunt.tasks.forEach(function(task){
+        var completedTasks = task.completedTasks;
+        completedTasks.forEach(function(completedTask){
+          console.log("completedTasks.userId= " + completedTask.userId + " req.body.user_id= "+req.body.user_id );
+          if(completedTask.userId == req.body.user_id){
+            result.completed.push(task);
+          } else {
+            result.open.push(task);
+          }
+        });
       });
-    };
-    
+      return res.status(200).send(result);
+    });
+  };
 });
 
 
@@ -48,7 +57,7 @@ router.post('/', function(req, res){
   var user = new User(req.body);
   user.save(function(error){
     if (error) return res.status(403).send({message: "User failed to create."})
-    return res.status(200).send(user);
+      return res.status(200).send(user);
   });
 });
 
@@ -56,7 +65,7 @@ router.get('/:id', function(req, res){
   var id = req.params.id;
   User.findById({_id: id}, function(error, user){
     if (error) return res.status(404).send({message: 'The user does not exist.'})
-    return res.status(200).send(user);
+      return res.status(200).send(user);
   });
 });
 
@@ -65,12 +74,12 @@ router.post('/:id', function(req, res){
   User.findById({_id: id}, function(error, user) {
     if (error) return res.status(404).send({message: 'The user does not exist.'})
 
-    if (req.body.full_name) user.full_name = req.body.full_name;
+      if (req.body.full_name) user.full_name = req.body.full_name;
     if (req.body.email) user.email         = req.body.email;
 
     user.save(function(error) {
       if (error) return res.status(500).send({message: "There seems to be some error in updating your user."})
-      return res.status(200).send(user);
+        return res.status(200).send(user);
     });
   });
 });
@@ -79,7 +88,7 @@ router.delete('/:id', function(req, res){
   var id = req.params.id;
   User.remove({_id: id}, function(error) {
     if (error) res.status(404).send({message: 'You seem to be mistaken, we have no user with that identity.'})
-    res.status(204);
+      res.status(204);
   });
   return;
 });
