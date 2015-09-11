975456fca90bfa0be9d1d9220e9cbdfa4f687d6d
keithdhd <keith@keithdouglas.co.uk>
2015-09-11 11:37:09 +0100

    create task model

---
+++ models/task.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+var taskSchema = new mongoose.Schema{(
+  title: String,
+  description: String,
+  points: Number
+  // type: {type: mongoose.Schema.ObjectId, ref: 'TaskType'}
+)}
+
+var Task = mongoose.model('Task', taskSchema);
+module.exports = Task;
+
